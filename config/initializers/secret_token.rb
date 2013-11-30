749601966568dac2616f692b781b42a6d2ee789a
LundWebfactory <fartcry@gmail.com>
2013-11-30 21:04:38 +0100

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Site::Application.config.secret_key_base = '290f0840ef72bd5e49a31aa4bfe083f824dfabcc04f47042cf7959549d3189392a36b40330afe5322115db3db8d912dd47202c13b0d7cee957941d25a477ad11'
