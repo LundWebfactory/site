f425606667f869bfca6efe0c2fe854e540e0c2cd
LundWebfactory <fartcry@gmail.com>
2013-12-01 20:27:40 +0100

    Signin/out done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      sign_in @user
+      flash[:success] = "You are now signed in!"
+      redirect_to root_path
     else
       render 'new'
     end
   end
-
   private
 
     def user_params
